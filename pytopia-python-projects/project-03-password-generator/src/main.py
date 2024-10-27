ca894217fe744baacfeff18ec316760ab812dfd3
Sozhan Natarajan <sozhan308@gmail.com>
2024-10-27 12:04:03 +0530

    update solution for password generator and include requirements.txt

--- pytopia-python-projects/project-03-password-generator/src/main.py
+++ pytopia-python-projects/project-03-password-generator/src/main.py
@@ -3,21 +3,45 @@
 from abc import ABC, abstractmethod
 import random
 import string
+import nltk
+from nltk.corpus import words
+from random import sample
 
 class PasswordGenerator(ABC):
   '''Abstract class for Password generation'''
   
   @abstractmethod
-  def PasswordGenerator(self, type):
+  def passwordGenerator(self, *args, **kwargs):
     self.type=type
   
 class RandomPasswordGenerator(PasswordGenerator):
   
-  def PasswordGenerator(self, length):
+  def passwordGenerator(self, length):
     combine_characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choices(combine_characters, k=length))
     return password
   
 
 class MemorablePasswordGenerator(PasswordGenerator):
-  pass
+  
+  def passwordGenerator(self, num_of_words, seperator):
+    nltk.download('words')
+    word_list = words.words()
+    memorable_passowrd = f'{seperator}'.join(sample(word_list,k=num_of_words))
+    return memorable_passowrd
+  
+class PinCodeGenerator(PasswordGenerator):
+  
+  def passwordGenerator(self, length):
+    pin_code = ''.join(random.choices(string.digits, k=length))
+    return pin_code
+    
+    
+def main():
+  # PG = PinCodeGenerator()
+  # PG = MemorablePasswordGenerator()
+  PG = RandomPasswordGenerator()
+  print(PG.passwordGenerator(10))
+  
+if __name__ == "__main__":
+  main()
